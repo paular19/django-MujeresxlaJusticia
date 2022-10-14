5d36795bcaf2fd8da5fb8db9e227e2e1442c0469
Paula Ramos <ramospaula@gmail.com>
2022-10-14 12:37:08 -0300

    ultimocambio

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-
+from django.conf.urls import handler404 
 
 
 
